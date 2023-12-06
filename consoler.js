b769c6524c49a909883cdc03dc2091824e75917e
taks14552 <62332268+taks14552@users.noreply.github.com>
2023-12-06 20:47:40 +0530

    Add files via upload

---
+++ consoler.js
@@ -0,0 +1,19 @@
+var a = 90
+var b = 5
+name1 = "Taikhum "
+name2 = "Bagasrawala"
+name3 = name1.concat(name2)
+
+console.log(name1)
+console.log(name2)
+console.log(name3)
+
+add = a + b
+div = a / b
+mul = a * b
+sub = a - b
+
+console.log(add)
+console.log(div)
+console.log(mul)
+console.log(sub)
